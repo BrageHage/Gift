16f395fe627d7ce9a7d89291f734a88713c032db
BrageHage <brage_hagelund@outlook.com>
2024-12-04 09:38:40 +0100

    All done

--- GiftPaige/src/App.tsx
+++ GiftPaige/src/App.tsx
@@ -1,9 +1,9 @@
-import './index.css';
-import confetti from 'canvas-confetti';
-import Song from './assets/Paige.mp3';
-import { useState, useEffect } from 'react';
-import cat from './assets/cat.gif';
-import cat2 from './assets/cat2.gif';
+import "./index.css";
+import confetti from "canvas-confetti";
+import Song from "./assets/Paige.mp3";
+import { useState, useEffect } from "react";
+import cat from "./assets/cat.gif";
+import cat2 from "./assets/cat2.gif";
 
 function App() {
   const [isPlaying, setIsPlaying] = useState(false);
@@ -11,15 +11,13 @@ function App() {
   const song = new Audio(Song);
   song.volume = 0.5;
 
-  // Add an event listener to reset isPlaying when the song ends
   useEffect(() => {
-    song.addEventListener('ended', () => {
+    song.addEventListener("ended", () => {
       setIsPlaying(false);
     });
 
-    // Cleanup to prevent memory leaks
     return () => {
-      song.removeEventListener('ended', () => {
+      song.removeEventListener("ended", () => {
         setIsPlaying(false);
       });
     };
@@ -29,13 +27,13 @@ function App() {
     confetti({
       particleCount: 100,
       spread: 70,
-      origin: { x: 0.5, y: 0.5 }, // Center of the screen
+      origin: { x: 0.5, y: 0.5 },
     });
 
     if (!isPlaying) {
       setIsPlaying(true);
       song.play().catch((error) => {
-        console.error('Error playing song:', error);
+        console.error("Error playing song:", error);
         setIsPlaying(false);
       });
     }
@@ -58,14 +56,7 @@ function App() {
 
       {isPlaying && (
         <>
-          {/* Cat2 GIF placed below the button */}
-          <img
-            className="absolute bottom-16"
-            src={cat2}
-            alt="Special Cat"
-          />
-
-          {/* 10 randomly placed Cat GIFs */}
+          <img className="absolute bottom-16" src={cat2} alt="Special Cat" />
           {Array.from({ length: 25 }).map((_, index) => (
             <img
               key={index}
